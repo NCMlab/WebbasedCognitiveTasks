fd472bafb29944b4f948e2805c68f88f290586ca
Jason Steffener - laptop <steffener@gmail.com>
2021-02-22 15:15:16 -0500

    Added Stroop ColorWord

---
+++ Stroop/Stroop_ColorWord.js
@@ -0,0 +1,98 @@
+var StroopColorWordList = [
+ {
+   "Word": "Red",
+   "Congruency": "Con",
+   "Correct": "v",
+   "Color": "(255,0,0)"
+ },
+ {
+   "Word": "Red",
+   "Congruency": "Incon",
+   "Correct": "b",
+   "Color": "(255,255,0)"
+ },
+ {
+   "Word": "Red",
+   "Congruency": "Incon",
+   "Correct": "m",
+   "Color": "(0,0,255)"
+ },
+ {
+   "Word": "Red",
+   "Congruency": "Incon",
+   "Correct": "n",
+   "Color": "(0,255,0)"
+ },
+ {
+   "Word": "Yellow",
+   "Congruency": "Incon",
+   "Correct": "v",
+   "Color": "(255,0,0)"
+ },
+ {
+   "Word": "Yellow",
+   "Congruency": "Con",
+   "Correct": "b",
+   "Color": "(255,255,0)"
+ },
+ {
+   "Word": "Yellow",
+   "Congruency": "Incon",
+   "Correct": "m",
+   "Color": "(0,0,255)"
+ },
+ {
+   "Word": "Yellow",
+   "Congruency": "Incon",
+   "Correct": "n",
+   "Color": "(0,255,0)"
+ },
+ {
+   "Word": "Blue",
+   "Congruency": "Incon",
+   "Correct": "v",
+   "Color": "(255,0,0)"
+ },
+ {
+   "Word": "Blue",
+   "Congruency": "Incon",
+   "Correct": "b",
+   "Color": "(255,255,0)"
+ },
+ {
+   "Word": "Blue",
+   "Congruency": "Con",
+   "Correct": "m",
+   "Color": "(0,0,255)"
+ },
+ {
+   "Word": "Blue",
+   "Congruency": "Incon",
+   "Correct": "n",
+   "Color": "(0,255,0)"
+ },
+ {
+   "Word": "Green",
+   "Congruency": "Incon",
+   "Correct": "v",
+   "Color": "(255,0,0)"
+ },
+ {
+   "Word": "Green",
+   "Congruency": "Incon",
+   "Correct": "b",
+   "Color": "(255,255,0)"
+ },
+ {
+   "Word": "Green",
+   "Congruency": "Incon",
+   "Correct": "m",
+   "Color": "(0,0,255)"
+ },
+ {
+   "Word": "Green",
+   "Congruency": "Con",
+   "Correct": "n",
+   "Color": "(0,255,0)"
+ }
+]
