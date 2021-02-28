8475270a63fefb750b2bc097b2fc14776dcb221f
Jason Steffener - laptop <steffener@gmail.com>
2021-02-28 10:37:12 -0500

    added instructions and jatosified the stroop

--- Stroop/Stroop_ColorWord.js
+++ Stroop/Stroop_ColorWord.js
@@ -1,6 +1,53 @@
 function PutIntoTable(top='top', middle='mid', width=600, height=300) {
       return '<table border="0" width="'+width+'"><tr height="'+height+'"><td>'+top+'</td></tr><tr height="'+height+'"><td><div style="font-size:60px;">'+middle+'</div></td></tr><tr height="'+height+'"><td valign="bottom"><img src="StroopColorsKeyboard.png"></td></tr></table>';
     }
+
+var StroopWordList = [
+   {
+      "Word": "Red",
+      "Correct": "v",
+      "Color": "(255,0,0)"
+    },
+    {
+      "Word": "Yellow",
+      "Correct": "b",
+      "Color": "(255,255,0)"
+    },
+    {
+      "Word": "Green",
+      "Correct": "n",
+      "Color": "(0,255,0)"
+    },
+    {
+      "Word": "Blue",
+      "Correct": "m",
+      "Color": "(0,0,255)"
+    }
+]
+/*
+var StroopColorList = [
+   {
+      "Rect": "<svg width="400" height="100"><rect width="400" height="100" style="fill:rgb(255,0,0);  stroke-width:3;stroke:rgb(0,0,0)" /></svg>",
+      "Correct": "v",
+      "Color": "Red"
+   },
+   {
+      "Rect": "<svg width="400" height="100"><rect width="400" height="100" style="fill:rgb(255,255,0);  stroke-width:3;stroke:rgb(0,0,0)" /></svg>",
+      "Correct": "b",
+      "Color": "Yellow"
+   },
+   {
+      "Rect": "<svg width="400" height="100"><rect width="400" height="100" style="fill:rgb(0,255,0);  stroke-width:3;stroke:rgb(0,0,0)" /></svg>",
+      "Correct": "n",
+      "Color": "Green"
+   },
+   {
+      "Rect": "<svg width="400" height="100"><rect width="400" height="100" style="fill:rgb(0,0,255);  stroke-width:3;stroke:rgb(0,0,0)" /></svg>",
+      "Correct": "m",
+      "Color": "Blue"
+   }
+]
+*/
 var StroopColorWordList = [
  {
    "Word": "Red",
