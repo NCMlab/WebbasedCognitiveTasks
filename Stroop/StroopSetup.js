cc7ae2656859368cd2da39d68e9551934b6894dc
Jason Steffener - laptop <steffener@gmail.com>
2021-03-02 09:35:32 -0500

    working on the use of a setup file

---
+++ Stroop/StroopSetup.js
@@ -0,0 +1,30 @@
+var ColorWelcome = "Welcome to the Stroop experiment. <p>In this experiment you will be identifying the color of the object that you see. You will see either a rectangular or a word.</p> Press any key to begin. "
+
+var WordWelcome = "Welcome to the Stroop experiment. <p>In this experiment you will be identifying the color of the object that you see. You will see either a rectangular or a word.</p> Press any key to begin. "
+
+var ColorWordWelcome = "Welcome to the Stroop experiment. <p>In this experiment you will be identifying the color of the object that you see. You will see either a rectangular or a word.</p> Press any key to begin. "
+
+var ColorPracticeInstructions = ""
+var WordPracticeInstructions = ""
+var ColorWordPracticeInstructions = ""
+
+var ColorTestInstructions = ""
+var WordTestInstructions = ""
+var ColorWordTestInstructions = ""
+
+var ThankYouText = ""
+
+var ResponseChoices = ['v','b','n','m',27]
+
+var FeedbackLength = 400; // This is in milliseconds
+
+var ColorPracticeRepeats = 1
+var WordPracticeRepeats = 1
+var ColorWordPracticeRepeats = 1
+
+// Since there are 4 possible trials, the number of trials will be 4 times the number of repeats
+var ColorTestRepeats = 1
+// Since there are 4 possible trials, the number of trials will be 4 times the number of repeats
+var WordTestRepeats = 1
+// Since there are 16 possible trials, the number of trials will be 4 times the number of repeats
+var ColorWordTestRepeats = 1
